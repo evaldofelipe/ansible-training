ae9adaf0b1f591290e6229b444d55f6e31c8dca9
evaldofelipe <contato@evaldofelipe.com>
2019-02-04 15:35:20 -0200

    First commit

---
+++ terraform/tester-environment.tf
@@ -0,0 +1,102 @@
+# Set provider and terraform version
+
+provider "azurerm" {
+  version = "~> 1.1"
+}
+
+terraform {
+  required_version = "0.11.5"
+}
+
+#Create resource group
+
+resource "azurerm_resource_group" "tmp_rg" {
+  name     = "${var.tmp_resource_group_name}"
+  location = "${var.location}"
+}
+
+# Create Vnet
+
+resource "azurerm_virtual_network" "tmp_vnet" {
+  name                = "${var.prefix}-vnet"
+  address_space       = ["${var.tmp_vnet}"]
+  location            = "${var.location}"
+  resource_group_name = "${azurerm_resource_group.tmp_rg.name}"
+}
+
+# Create Subnet
+
+resource "azurerm_subnet" "tmp_subnet" {
+  name                 = "${var.prefix}-subnet"
+  resource_group_name  = "${azurerm_resource_group.tmp_rg.name}"
+  virtual_network_name = "${azurerm_virtual_network.tmp_vnet.name}"
+  address_prefix       = "${var.tmp_subnet}"
+}
+
+# Provisioning public IP
+
+resource "azurerm_public_ip" "tmp_public_ip" {
+  name                         = "${var.prefix}-public-ip"
+  location                     = "${var.location}"
+  resource_group_name          = "${azurerm_resource_group.tmp_rg.name}"
+  public_ip_address_allocation = "dynamic"
+}
+
+# Provisioning eth (w/ public IP)
+
+resource "azurerm_network_interface" "tmp_nic" {
+  name                = "${var.tmp_nic_name}"
+  location            = "${var.location}"
+  resource_group_name = "${azurerm_resource_group.tmp_rg.name}"
+
+  ip_configuration {
+    name                          = "nic-config"
+    subnet_id                     = "${azurerm_subnet.tmp_subnet.id}"
+    private_ip_address_allocation = "dynamic"
+    public_ip_address_id          = "${azurerm_public_ip.tmp_public_ip.id}"
+  }
+}
+
+# Create virutal machine
+
+resource "azurerm_virtual_machine" "tmp_vm" {
+  name                  = "${var.prefix}-vm"
+  location              = "${var.location}"
+  resource_group_name   = "${azurerm_resource_group.tmp_rg.name}"
+  network_interface_ids = ["${azurerm_network_interface.tmp_nic.id}"]
+  vm_size               = "${var.tmp_vm_size}"
+
+  # This means the OS Disk will be deleted when Terraform destroys the Virtual Machine
+  # NOTE: This is used here only for a tmp environment
+  delete_os_disk_on_termination = true
+
+
+  storage_os_disk {
+    name              = "osdisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  storage_image_reference {
+  publisher = "${var.image_publisher}"
+  offer     = "${var.image_offer}"
+  sku       = "${var.image_sku}"
+  version   = "${var.image_version}"
+  }
+
+  os_profile {
+    admin_username = "${var.tmp_user}"
+    admin_password = ""
+    computer_name  = "${var.prefix}-vm"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = true
+
+    ssh_keys {
+      path     = "/home/${var.tmp_user}/.ssh/authorized_keys"
+      key_data = "${file("~/.ssh/id_rsa.pub")}"
+    }
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,91 @@
+# Global variables
+
+variable "location" {
+  description = "The Azure Region in which the resources should exist"
+  default     = "eastus2"
+}
+
+# Builder variables
+
+variable "builder_resource_group_name" {
+  description = "The name of the resouce groupe used by packer"
+  default     = "tmp-resource-group"
+}
+
+variable "builder_image_name" {
+  description = "Image name provisioned by packer"
+  default     = "ubuntu-neoway-image"
+}
+
+# tmp variables
+
+variable "prefix" {
+  description = "The Prefix used for all resources"
+  default     = "tmp"
+}
+
+variable "tmp_env" {
+  description = "Default name for builder environment"
+  default     = "tmp"
+}
+
+variable "tmp_resource_group_name" {
+  description = "The name of the resouce groupe used by tmp"
+  default     = "tmp-resource-group"
+}
+
+variable "tmp_vnet" {
+  description = "Address for tmp vnet"
+  default     = "10.181.0.0/16"
+}
+
+variable "tmp_subnet" {
+  description = "Address for tmp subnet"
+  default     = "10.181.1.0/24"
+}
+
+variable "tmp_nic_name" {
+  description = "Nic tmp name"
+  default     = "tmp-nic"
+}
+
+variable "tmp_vm_name" {
+  description = "tmp vm name"
+  default     = "tmp-vm"
+}
+
+variable "tmp_user" {
+  description = "user for tmp vm"
+  default     = "packer"
+}
+
+variable "tmp_vm_size" {
+  description = "Azure vm size"
+  default     = "Standard_A0"
+}
+
+variable "vhd_url" {
+  description = "VHD url created by packer"
+  default     = ""
+}
+
+variable "image_publisher" {
+  description = ""
+  default = "Canonical"
+}
+
+variable "image_offer" {
+  description = ""
+  default = "UbuntuServer"
+}
+
+
+variable "image_sku" {
+  description = ""
+  default = "16.04-LTS"
+}
+
+variable "image_version" {
+  description = ""
+  default = "latest"
+}
