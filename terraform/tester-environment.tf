50d98f19230634a26582ae05ea4f4551a2d0e0da
evaldofelipe <contato@evaldofelipe.com>
2019-02-07 10:17:37 -0200

    Remove unused output

--- terraform/tester-environment.tf
+++ terraform/tester-environment.tf
@@ -105,7 +105,3 @@ output "public_ip_id" {
   description = "id of the public ip address provisoned."
   value       = "${azurerm_public_ip.tmp_public_ip.*.id}"
 }
-output "public_ip_address" {
-  description = "The actual ip address allocated for the resource."
-  value       = "${azurerm_public_ip.tmp_public_ip.*.ip_address}"
-}
